96e7b1d893f1ed1b185a2d14b3ba17ee1f19b78e
Louie Adamian <louieadamian@gmail.com>
2016-05-19 09:15:56 -0400

    adds upload function

--- exec.js
+++ exec.js
@@ -1,30 +1,27 @@
 var app = require('express')()
-  //  .use(siofu.router)
-  //    .listen(5000);
+    //  .use(siofu.router)
+    //    .listen(5000);
 var uuid = require('uuid')
+var mkdirp = require('mkdirp')
+var uuid = uuid.v4()
 var exec = require("child_process").exec
 var siofu = require("socketio-file-upload")
-var upload = false
-app.set('view engine', 'pug');
+app.set('view engine', 'pug')
 app.get('/', function(req, res) {
-  res.render("index")
+    res.render("index")
 })
 
-function upload() {
-fs.mkdirs'/jobs/uuid.json'
-
-}
 /*var command = "cd C:/Users/louieadamian/Documents/slic3r-mswin-x64-1-2-9a-stable/Slic3r\n"
     /*var command = "cd C:/Users/louieadamian/Documents/slic3r-mswin-x64-1-2-9a-stable/Slic3r\n"
     command += "slic3r-console.exe\n"
     command += "slic3r teensy.stl --output slic3r-mswin-x64-1-2-9a-stable/Slic3r"
      console.log(command);*/
-    //console.log("cd C:/Users/louieadamian/Documents/slic3r-mswin-x64-1-2-9a-stable/Slic3r\nslic3r-console.exe\nslic3r teensy.stl --output slic3r-mswin-x64-1-2-9a-stable/Slic3r");
+//console.log("cd C:/Users/louieadamian/Documents/slic3r-mswin-x64-1-2-9a-stable/Slic3r\nslic3r-console.exe\nslic3r teensy.stl --output slic3r-mswin-x64-1-2-9a-stable/Slic3r");
 exec("cd C:/Users/louieadamian/Documents/slic3r-mswin-x64-1-2-9a-stable/Slic3r\nslic3r-console.exe\nslic3r teensy.stl --output slic3r-mswin-x64-1-2-9a-stable/Slic3r", function(err, out) {
     if (err) {
         throw err
     }
-
+    upload()
     console.log("finished")
     console.log(out)
 })
@@ -32,3 +29,10 @@ exec("cd C:/Users/louieadamian/Documents/slic3r-mswin-x64-1-2-9a-stable/Slic3r\n
 app.listen(5000, () => {
     console.log('3D printer station is listening on port 5000')
 })
+
+function upload() {
+    fs.mkdirs('/jobs/', function(err) {
+        if (err) {
+            console.log(err);
+        }
+    })
