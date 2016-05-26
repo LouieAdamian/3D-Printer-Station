22ad7b0caf09f470f3cff876857d2bcc7b43f5f1
sdaitzman <samuel@daitzman.com>
2016-05-26 09:56:40 -0400

    Revert "Merge origin/master into master"
    This reverts commit 733ba2e1e5d5fffdfe2e8e08d25ecf688688acb7, reversing
    changes made to 36b78247f11595b9061b0d7f9d48db422779d156.

--- exec.js
+++ exec.js
@@ -1,47 +1,34 @@
 var app = require('express')()
-    //  .use(siofu.router)
-    //    .listen(5000);
+  //  .use(siofu.router)
+  //    .listen(5000);
 var uuid = require('uuid')
-// var fs = require('fs.extra')
-var mkdirp = require('mkdirp')
 var exec = require("child_process").exec
 var siofu = require("socketio-file-upload")
-app.set('view engine', 'pug')
+var upload = false
+app.set('view engine', 'pug');
 app.get('/', function(req, res) {
-    res.render("index")
+  res.render("index")
 })
 
+function upload() {
+fs.mkdirs'/jobs/uuid.json'
+
+}
+/*var command = "cd C:/Users/louieadamian/Documents/slic3r-mswin-x64-1-2-9a-stable/Slic3r\n"
+    /*var command = "cd C:/Users/louieadamian/Documents/slic3r-mswin-x64-1-2-9a-stable/Slic3r\n"
+    command += "slic3r-console.exe\n"
+    command += "slic3r teensy.stl --output slic3r-mswin-x64-1-2-9a-stable/Slic3r"
+     console.log(command);*/
+    //console.log("cd C:/Users/louieadamian/Documents/slic3r-mswin-x64-1-2-9a-stable/Slic3r\nslic3r-console.exe\nslic3r teensy.stl --output slic3r-mswin-x64-1-2-9a-stable/Slic3r");
 exec("cd C:/Users/louieadamian/Documents/slic3r-mswin-x64-1-2-9a-stable/Slic3r\nslic3r-console.exe\nslic3r teensy.stl --output slic3r-mswin-x64-1-2-9a-stable/Slic3r", function(err, out) {
     if (err) {
         throw err
     }
-    upload()
+
     console.log("finished")
     console.log(out)
 })
-app.listen(6969, () => {
-    console.log('3D printer station is listening on port 6969')
+
+app.listen(5000, () => {
+    console.log('3D printer station is listening on port 5000')
 })
-
-function upload(stl) {
-    var path = __dirname + "/jobs/"
-    var stluuid = uuid.v4()
-    var jobuuid = uuid.v4()
-    var gcoudeuuid = uuid.v4()
-    var folder = path+jobuuid+'/'
-    fs.mkdirs(folder, function(err) {
-        if (err) {
-            console.log(err);
-        }
-    })
-    fs.copy(stl, folder + suuid + ".stl", function(e) {
-        if (e) {
-            console.log(e);
-            loadViewer()
-        }
-    })
-}
-
-function loadViewer() {
-
-}
